ba7bcdf62f30ca66f5f0177eef0cfe1d5aba2171
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2025-09-20 18:37:55 +0100

    Add accessors to Authentication

--- src/installer/authentication/mod.rs
+++ src/installer/authentication/mod.rs
@@ -11,7 +11,7 @@ pub struct Authentication {
     /// This field controls whether to use no authentication, Entra ID, or Entra ID for Azure
     /// Blob Storage.
     #[cfg_attr(feature = "serde", serde(rename = "AuthenticationType"))]
-    pub r#type: AuthenticationType,
+    r#type: AuthenticationType,
 
     /// This field controls the authentication details used when downloading or installing packages
     /// from Entra Id secured private sources.
@@ -19,5 +19,25 @@ pub struct Authentication {
         feature = "serde",
         serde(skip_serializing_if = "MicrosoftEntraIdAuthenticationInfo::is_empty")
     )]
-    pub microsoft_entra_id_authentication_info: MicrosoftEntraIdAuthenticationInfo,
+    microsoft_entra_id_authentication_info: MicrosoftEntraIdAuthenticationInfo,
+}
+
+impl Authentication {
+    /// Returns the authentication type which controls whether to use no authentication, Entra ID,
+    /// or Entra ID for Azure Blob Storage.
+    #[must_use]
+    #[inline]
+    pub const fn r#type(&self) -> AuthenticationType {
+        self.r#type
+    }
+
+    /// Returns the Microsoft Entra ID authentication info which controls the authentication details
+    /// used when downloading or installing packages from Entra Id secured private sources.
+    #[must_use]
+    #[inline]
+    pub const fn microsoft_entra_id_authentication_info(
+        &self,
+    ) -> &MicrosoftEntraIdAuthenticationInfo {
+        &self.microsoft_entra_id_authentication_info
+    }
 }
